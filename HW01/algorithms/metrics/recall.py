a059311f7705a39977a2bed2e5536812ae21bbc8
Ayman <98105746+aymanbnz@users.noreply.github.com>
2024-09-02 15:16:58 -0500

    Update recall.py

--- HW01/algorithms/metrics/recall.py
+++ HW01/algorithms/metrics/recall.py
@@ -1,5 +1,7 @@
 
 
 def recall_at_k(relevance_query: list , number_relevant_docs:int, k:int):
-
-    return
+    query_at_k = relevance_query[:k]
+    relevant = query_at_k.count(1)
+    print(relevant/number_relevant_docs)
+    return(relevant/number_relevant_docs)
