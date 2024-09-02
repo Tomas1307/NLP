5c054bbad8db290c93b0655e99aec9c2319b44e7
Ayman <98105746+aymanbnz@users.noreply.github.com>
2024-09-02 15:13:11 -0500

    Update precision.py

--- HW01/algorithms/metrics/precision.py
+++ HW01/algorithms/metrics/precision.py
@@ -1,7 +1,13 @@
 import numpy
 
 def precision(relevance_query: list):
-        return
+        all_docs = len(relevance_query)
+        pertinent = relevance_query.count(1)
+        print(pertinent/all_docs)
+        return (pertinent/all_docs)
     
 def precision_at_k(relevance_query: list ,k: int):
-    return
+    query_at_k = relevance_query[:k]
+    all_docs= len(query_at_k)
+    relevant = query_at_k.count(1)
+    return (relevant/all_docs)
