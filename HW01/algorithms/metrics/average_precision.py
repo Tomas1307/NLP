71f785381be1a67b74289bdab62b6f10871b32c0
Ayman <98105746+aymanbnz@users.noreply.github.com>
2024-09-02 15:22:20 -0500

    Update average_precision.py

--- HW01/algorithms/metrics/average_precision.py
+++ HW01/algorithms/metrics/average_precision.py
@@ -1,2 +1,13 @@
 def average_precision(relevance_query: list):
-    return
+    nb_relevant = relevance_query.count(1)
+    nb_relevant_found = 0
+    sum_precisions = 0
+    k=0
+    while nb_relevant!=nb_relevant_found:
+        if relevance_query[k]==1:
+            nb_relevant_found+=1
+            sum_precisions += precision_at_k(relevance_query, k+1)
+        k+=1
+        
+    print (sum_precisions/nb_relevant)
+    return (sum_precisions/nb_relevant)
