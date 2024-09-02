d200a4de89f9cccfb14988c0e0869f853267f8ed
Ayman <98105746+aymanbnz@users.noreply.github.com>
2024-09-02 15:23:46 -0500

    Update mean_average_precision.py

--- HW01/algorithms/metrics/mean_average_precision.py
+++ HW01/algorithms/metrics/mean_average_precision.py
@@ -1,2 +1,6 @@
 def MAP(relevance_query: list):
-    return
+    map_value = 0
+    for i in range (len(relevance_query)):
+        map_value+=average_precision(relevance_query[i])
+    print (map_value/len(relevance_query))
+    return (map_value/len(relevance_query))
