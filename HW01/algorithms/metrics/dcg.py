afcf55360edda261c165bd327d26697b23462390
Gotty <78111224+iGotty@users.noreply.github.com>
2024-09-03 15:50:07 -0500

    dcg

--- HW01/algorithms/metrics/dcg.py
+++ HW01/algorithms/metrics/dcg.py
@@ -2,7 +2,6 @@ import numpy as np
 
 def dcg_at_k(relevance_query, k):
     relevance_query = np.array(relevance_query)[:k]
-    # Implementamos max(i, 2) en el cálculo de descuentos
     discounts = np.log2(np.maximum(np.arange(1, k + 1), 2))
     dcg = np.sum(relevance_query / discounts)
     return dcg
--- HW01/algorithms/metrics/ndcg.py
+++ HW01/algorithms/metrics/ndcg.py
@@ -2,7 +2,6 @@ from .dcg import dcg_at_k
 
 def ndcg_at_k(relevance_query: list, k: int):
     dcg = dcg_at_k(relevance_query, k)
-    # Ordenamos las relevancias en orden descendente para obtener el IDCG
     sorted_relevance_query = sorted(relevance_query, reverse=True)
     idcg = dcg_at_k(sorted_relevance_query, k)
     ndcg = dcg / idcg if idcg > 0 else 0
