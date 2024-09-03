d4ac54c6ca1a8528b8da98609d92db5c1b258b6c
Gotty <78111224+iGotty@users.noreply.github.com>
2024-09-03 15:48:53 -0500

    punto_1_completo

--- HW01/algorithms/metrics/dcg.py
+++ HW01/algorithms/metrics/dcg.py
@@ -1,6 +1,8 @@
-def dcg_at_k(relevance_query,k):
-    dcg = 0
-    for i in range(k):
-        dcg = dcg + (relevance_query[i] / math.log2( max(i+1, 2)))
-    print dcg
+import numpy as np
+
+def dcg_at_k(relevance_query, k):
+    relevance_query = np.array(relevance_query)[:k]
+    # Implementamos max(i, 2) en el cálculo de descuentos
+    discounts = np.log2(np.maximum(np.arange(1, k + 1), 2))
+    dcg = np.sum(relevance_query / discounts)
     return dcg
